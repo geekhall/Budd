374c29166c09853acbe841c60d33352a43e515bf
geekhall <174821300@qq.com>
2021-11-20 21:11:04 +0800

    20211120-211104

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,15 +3,18 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
 import Demo from './components/Demo.vue';
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Sample from './components/Sample.vue';
 
 </script>
 
 <template>
+<Header></Header>
 
 <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
-  <Demo></Demo>
-
+<Footer></Footer>
 
 </template>
 
@@ -22,6 +25,11 @@ import Demo from './components/Demo.vue';
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+body {
+  margin: 0;
+  padding: 0;
+}
+
 </style>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <el-affix position="bottom" :offset="0" class="footer">
+      asdfasdf
+  </el-affix>
+</template>
+
+<style scoped>
+.footer {
+  background-color: #565c63;
+}
+</style>
